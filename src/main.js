01519ad5d36fc80b4d95c970e759d2063a7ccf35
ecrseer <juustino@live.com>
2021-09-15 21:43:51 -0300

    vue router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import store from './store'
 import mitt from 'mitt'
-
+import rotas from './rotas'
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
@@ -10,5 +10,6 @@ import './style/estilo.css'
 const bus = mitt()
 const app = createApp(App)
 app.use(store)
+app.use(rotas)
 app.config.globalProperties.$bus = bus
 app.mount('#app')
--- src/rotas.js
+++ src/rotas.js
@@ -1,11 +1,9 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-import Home from './pages/Home.vue' 
-import Detalhes from './pages/Detalhes.vue'
+import Home from './pages/Home.vue'  
 
 const routes = [
-  { path: '/', name:'home', component: Home }, 
-  { path: '/detalhes-time', name:'detalhes', component: Detalhes },/*
+  { path: '/', name:'home', component: Home },  /*
   { path: '/filme/:id', name:'detalhes', component: Detalhes }, */
 ]
 
